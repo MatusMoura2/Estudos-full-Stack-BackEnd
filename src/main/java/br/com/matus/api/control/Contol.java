fb68f141579ac9f91ff7e10eb14326477fdac336
Matus Moura <matus.moura333x2@gmail.com>
2024-02-11 15:24:15 -0300

    implementando metodo delete

--- src/main/java/br/com/matus/api/control/Contol.java
+++ src/main/java/br/com/matus/api/control/Contol.java
@@ -1,8 +1,11 @@
 package br.com.matus.api.control;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +26,16 @@ public class Contol {
 	@PostMapping("/")
 	public Client register(@RequestBody Client c) {
 		return action.save(c);
+	}
+	
+	@PutMapping("/")
+	public Client edit(@RequestBody Client c) {
+		return action.save(c);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		action.deleteById(id);
 	}
 	
 }
